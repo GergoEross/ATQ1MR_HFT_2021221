fc18834b738ac3ac0e9964b11af728ed7b90f025
erossgergo11@stud.uni-obuda.hu <erossgergo11@stud.uni-obuda.hu>
2021-10-14 12:41:19 +0200

    Added PcPartsDbContext class

---
+++ ATQ1MR_HFT_2021221.Data/PcPartsDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ATQ1MR_HFT_2021221.Data
+{
+    public class PcPartsDbContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseLazyLoadingProxies().UseSqlServer(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\PcPartsDb.mdf;Integrated Security=true;MultipleActiveResultSets=True");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
