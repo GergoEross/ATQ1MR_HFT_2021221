117979d306509ae5bdd5a8d7d4375ec465a754bc
erossgergo11@stud.uni-obuda.hu <erossgergo11@stud.uni-obuda.hu>
2021-10-14 13:37:16 +0200

    Added content to Processor

--- ATQ1MR_HFT_2021221.Models/Entities/Processor.cs
+++ ATQ1MR_HFT_2021221.Models/Entities/Processor.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ATQ1MR_HFT_2021221.Models.Entities
 {
+    [Table("Processors")]
     public class Processor
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(15)]
+        public string Socket { get; set; }
+        [Required]
+        [MaxLength(25)]
+        public string Name { get; set; }
+        public int BaseClock { get; set; }
+        public int BoostClock { get; set; }
+        public int Cores { get; set; }
+        public int Threads { get; set; }
+        public int Price { get; set; }
+        [NotMapped]
+        public virtual PBrand Brand { get; set; }
+
     }
 }
