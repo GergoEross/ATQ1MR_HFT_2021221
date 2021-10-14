f8991e170c459542c2e8297d2faed3077ca1ae8f
erossgergo11@stud.uni-obuda.hu <erossgergo11@stud.uni-obuda.hu>
2021-10-14 13:26:58 +0200

    Added content to Motherboard

--- ATQ1MR_HFT_2021221.Models/Entities/Motherboard.cs
+++ ATQ1MR_HFT_2021221.Models/Entities/Motherboard.cs
@@ -1,12 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ATQ1MR_HFT_2021221.Models.Entities
 {
+    [Table("Motherboards")]
     public class Motherboard
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        public int Price { get; set; }
+        [Required]
+        [MaxLength(25)]
+        public string Type { get; set; }
+        [Required]
+        [MaxLength(15)]
+        public string Chipset { get; set; }
+        [Required]
+        [MaxLength(15)]
+        public string Socket { get; set; }
+        [NotMapped]
+        public virtual MBrand Brand { get; set; }
     }
 }
