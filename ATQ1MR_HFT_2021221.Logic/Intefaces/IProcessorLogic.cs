38023d9745bca0d55bc43ab0026b110f468ac694
erossgergo11@stud.uni-obuda.hu <erossgergo11@stud.uni-obuda.hu>
2021-11-19 11:01:19 +0100

    Added crud methodes to ProcessorLogic

--- ATQ1MR_HFT_2021221.Logic/Intefaces/IProcessorLogic.cs
+++ ATQ1MR_HFT_2021221.Logic/Intefaces/IProcessorLogic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ATQ1MR_HFT_2021221.Models.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace ATQ1MR_HFT_2021221.Logic.Intefaces
 {
     public interface IProcessorLogic
     {
+        IList<Processor> ReadAll();
+        Processor Read(int id);
+        Processor Create(Processor entity);
+        Processor Update(Processor entity);
+        void Delete(int id);
     }
 }
--- ATQ1MR_HFT_2021221.Logic/Services/ProcessorLogic.cs
+++ ATQ1MR_HFT_2021221.Logic/Services/ProcessorLogic.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ATQ1MR_HFT_2021221.Logic.Intefaces;
+using ATQ1MR_HFT_2021221.Models.Entities;
+using ATQ1MR_HFT_2021221.Repository.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,32 @@ using System.Threading.Tasks;
 
 namespace ATQ1MR_HFT_2021221.Logic.Services
 {
-    public class ProcessorLogic
+    public class ProcessorLogic : IProcessorLogic
     {
+        IPBrandRepository _pBrandRepository;
+        IMotherboardRepository _motherboardRepository;
+        IProcessorRepository _processorRepository;
+        public IList<Processor> ReadAll()
+        {
+            return _processorRepository.ReadAll().ToList();
+        }
+        public Processor Read(int id)
+        {
+            return _processorRepository.Read(id);
+        }
+        public Processor Create(Processor entity)
+        {
+            var result = _processorRepository.Create(entity);
+            return result;
+        }
+        public Processor Update(Processor entity)
+        {
+            var result = _processorRepository.Update(entity);
+            return result;
+        }
+        public void Delete(int id)
+        {
+            _processorRepository.Delete(id);
+        }
     }
 }
