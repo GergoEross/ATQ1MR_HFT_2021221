6228c4d7aef82342502090818e282e819a5e06da
erossgergo11@stud.uni-obuda.hu <erossgergo11@stud.uni-obuda.hu>
2021-11-19 11:24:41 +0100

    Added crud methodes to MBrandLogic

--- ATQ1MR_HFT_2021221.Logic/Intefaces/IMBrandLogic.cs
+++ ATQ1MR_HFT_2021221.Logic/Intefaces/IMBrandLogic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ATQ1MR_HFT_2021221.Models.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace ATQ1MR_HFT_2021221.Logic.Intefaces
 {
     public interface IMBrandLogic
     {
+        IList<MBrand> ReadAll();
+        MBrand Read(int id);
+        MBrand Create(MBrand entity);
+        MBrand Update(MBrand entity);
+        void Delete(int id);
     }
 }
--- ATQ1MR_HFT_2021221.Logic/Services/MBrandLogic.cs
+++ ATQ1MR_HFT_2021221.Logic/Services/MBrandLogic.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ATQ1MR_HFT_2021221.Logic.Intefaces;
+using ATQ1MR_HFT_2021221.Models.Entities;
+using ATQ1MR_HFT_2021221.Repository.Interfaces;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,48 @@ using System.Threading.Tasks;
 
 namespace ATQ1MR_HFT_2021221.Logic.Services
 {
-    public class MBrandLogic
+    public class MBrandLogic : IMBrandLogic
     {
+        IMBrandRepository _mBrandRepository;
+        IMotherboardRepository _motherboardRepository;
+        IProcessorRepository _processorRepository;
+        public IList<MBrand> ReadAll()
+        {
+            return _mBrandRepository.ReadAll().ToList();
+        }
+        public MBrand Read(int id)
+        {
+            return _mBrandRepository.Read(id);
+        }
+        public MBrand Create(MBrand entity)
+        {
+            var v = _mBrandRepository.Read(entity.Id);
+            if (v == null)
+            {
+                var result = _mBrandRepository.Create(entity);
+                return result;
+            }
+            else
+            {
+                throw new Exception("Already exists!");
+            }
+        }
+        public MBrand Update(MBrand entity)
+        {
+            var v = _mBrandRepository.Read(entity.Id);
+            if (v != null)
+            {
+                var result = _mBrandRepository.Update(entity);
+                return result;
+            }
+            else
+            {
+                throw new Exception("No entity found!");
+            }   
+        }
+        public void Delete(int id)
+        {
+            _mBrandRepository.Delete(id);
+        }
     }
 }
