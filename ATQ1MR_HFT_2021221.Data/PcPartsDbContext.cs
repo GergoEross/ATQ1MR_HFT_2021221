3d0366aa28cd95bd4f51239ecd799b635d9de75f
erossgergo11@stud.uni-obuda.hu <erossgergo11@stud.uni-obuda.hu>
2021-12-01 15:11:55 +0100

    Added more seed data

--- ATQ1MR_HFT_2021221.Data/PcPartsDbContext.cs
+++ ATQ1MR_HFT_2021221.Data/PcPartsDbContext.cs
@@ -33,18 +33,22 @@ namespace ATQ1MR_HFT_2021221.Data
             //Seed
             var msi = new MBrand() { Id = 1, Name = "MSI" };
             var asus = new MBrand() { Id = 2, Name = "Asus" };
+            var gigabyte = new MBrand() { Id = 3, Name = "GIGABYTE" };
             var intel = new PBrand() { Id = 1, Name = "Intel" };
             var amd = new PBrand() { Id = 2, Name = "AMD" };
 
             var mboard1 = new Motherboard() { Id = 1, BrandId = msi.Id, Chipset = "B450", Price = 30000, Socket = "AM4", Type = "TOMAHAWK MAX" };
             var mboard2 = new Motherboard() { Id = 2, BrandId = asus.Id, Chipset = "Z390", Price = 40000, Socket = "LGA-1151(300)", Type = "MPG GAMING PLUS" };
+            var mboard3 = new Motherboard() { Id = 2, BrandId = gigabyte.Id, Chipset = "B450", Price = 32000, Socket = "AM4", Type = "AORUS PRO" };
             var cpu1 = new Processor() { Id = 1, BrandId = amd.Id, Name = "Ryzen 5 3600", Socket = "AM4", Cores = 6, Threads = 12, BaseClock = 3.6, BoostClock = 4.2, Price = 110000 };
             var cpu2 = new Processor() { Id = 2, BrandId = intel.Id, Name = "Core i9-9900K", Socket = "LGA-1151(300)", Cores = 8, Threads = 16, BaseClock = 3.6, BoostClock = 5, Price = 134000 };
+            var cpu3 = new Processor() { Id = 2, BrandId = amd.Id, Name = "Ryzen 5 5600X", Socket = "AM4", Cores = 6, Threads = 12, BaseClock = 3.7, BoostClock = 4.6, Price = 122000 };
+            var cpu4 = new Processor() { Id = 2, BrandId = intel.Id, Name = "Core i5-9400F", Socket = "LGA-1151(300)", Cores = 6, Threads = 6, BaseClock = 2.9, BoostClock = 4.1, Price = 57000 };
 
-            modelBuilder.Entity<MBrand>().HasData(msi, asus);
+            modelBuilder.Entity<MBrand>().HasData(msi, asus, gigabyte);
             modelBuilder.Entity<PBrand>().HasData(intel, amd);
-            modelBuilder.Entity<Motherboard>().HasData(mboard1, mboard2);
-            modelBuilder.Entity<Processor>().HasData(cpu1, cpu2);
+            modelBuilder.Entity<Motherboard>().HasData(mboard1, mboard2, mboard3);
+            modelBuilder.Entity<Processor>().HasData(cpu1, cpu2, cpu3, cpu4);
         }
     }
 }
